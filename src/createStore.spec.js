4b16d9dc62e92d70c70eb4cb85bdfcf11829f940
David Sergey <nirth.furzahad@gmail.com>
2017-06-25 22:56:37 +0100

    Added test cases for subscription and unsubscription.

--- src/createStore.spec.js
+++ src/createStore.spec.js
@@ -1,10 +1,15 @@
-import chai, {expect} from 'chai';
-import dirtyChai from 'dirty-chai';
+import chai, {expect} from 'chai'
+import dirtyChai from 'dirty-chai'
+import sinonChai from 'sinon-chai'
+import sinonChaiInOrder from 'sinon-chai-in-order'
+import {spy} from 'sinon'
 
 import {allowEventType} from './utils';
 import {createStore} from './createStore';
 
-chai.use(dirtyChai);
+chai.use(dirtyChai)
+chai.use(sinonChai)
+chai.use(sinonChaiInOrder)
 
 const mapInitialization = () => true;
 const mapAge = ({payload: age}) => age;
@@ -71,5 +76,75 @@ describe('createStore should', () => {
         age: 22,
       },
     })
+  })
+
+  it('dispatch new events and allow subscriptions to receive updated state', () => {
+    const {initialState, eventMap, events: {
+      initialize,
+      updateFirstName,
+      updateLastName,
+      updateAge,
+    }} = stateFixture()
+
+    const store = createStore(eventMap, initialState)
+    const onNextState = spy()
+
+    store.subscribe(onNextState)
+    store.dispatch(initialize())
+
+    store.dispatch(updateAge(32))
+    store.dispatch(updateFirstName('Malika'))
+    store.dispatch(updateLastName('Favre'))
+    store.dispatch(updateAge(22))
+
+    expect(onNextState).inOrder
+      .subsequently.calledWith({
+        initialized: true,
+        user: {name: {firstName: null, lastName: null}, age: null},
+      })
+      .subsequently.calledWith({
+        initialized: true,
+        user: {name: {firstName: null, lastName: null}, age: 32},
+      })
+      .subsequently.calledWith({
+        initialized: true,
+        user: {name: {firstName: 'Malika', lastName: null}, age: 32},
+      })
+      .subsequently.calledWith({
+        initialized: true,
+        user: {name: {firstName: 'Malika', lastName: 'Favre'}, age: 32},
+      })
+      .subsequently.calledWith({
+        initialized: true,
+        user: {name: {firstName: 'Malika', lastName: 'Favre'}, age: 22},
+      })
+  })
+
+  it('dispatch new events and allow subscribers to unsubscribe', () => {
+    const {initialState, eventMap, events: {
+      initialize,
+      updateFirstName,
+      updateLastName,
+      updateAge,
+    }} = stateFixture()
+
+    const store = createStore(eventMap, initialState)
+    const onNextState = spy()
+
+    const subscription = store.subscribe(onNextState)
+    store.dispatch(initialize())
+
+    store.dispatch(updateAge(32))
+    store.dispatch(updateFirstName('Malika'))
+    
+    subscription.unsubscribe()
+
+    store.dispatch(updateLastName('Favre'))
+    store.dispatch(updateAge(22))
+
+    expect(onNextState).inOrder
+      .subsequently.calledWith({initialized: true, user: {name: {firstName: null, lastName: null}, age: null}})
+      .subsequently.calledWith({initialized: true, user: {name: {firstName: null, lastName: null}, age: 32}})
+      .subsequently.not.called()
   })
 });
