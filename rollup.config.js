7ca974267999e28b6ecaacc6ad4abc7aa00e9791
David Sergey <davidsergey@Davids-MacBook-Pro.local>
2018-04-22 10:51:46 +0100

    Migrated project to Yarn. Also because of the Flow, now project requires compilation, hence - added rollup

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+const flow = require('rollup-plugin-flow')
+const uglify = require('rollup-plugin-uglify')
+
+export default {
+  entry: 'src/index.js',
+  format: 'cjs',
+  dest: './lib/lib.js',
+  plugins: [flow({pretty: true})],
+  external: ['ramda'],
+}
