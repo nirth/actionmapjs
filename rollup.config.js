cac1df190a8821ae602517e8ad189aac681a942e
David Sergey <davidsergey@Davids-MacBook-Pro.local>
2018-04-22 11:02:28 +0100

    Boring update: Updated rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,8 @@ const flow = require('rollup-plugin-flow')
 const uglify = require('rollup-plugin-uglify')
 
 export default {
-  entry: 'src/index.js',
-  format: 'cjs',
-  dest: './lib/lib.js',
-  plugins: [flow({pretty: true})],
-  external: ['ramda'],
+  input: 'src/index.js',
+  output: {format: 'cjs', file: './lib/lib.js'},
+  plugins: [flow({pretty: true}), uglify()],
+  external: ['ramda', 'rxjs', 'immutable'],
 }
