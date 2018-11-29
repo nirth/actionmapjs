bd3a09bcc08af656ede62d0e971b16c72f6c3609
David Grigoryan <d4@h3x.co>
2018-11-29 22:35:02 +0000

    Fancy error reporting

--- matejs/src/createNextState.js
+++ matejs/src/createNextState.js
@@ -35,13 +35,13 @@ const evaluateGuard = (guard: Guard, event: Event): boolean => {
     return guard(event)
   }
 
-  throw new Error(
-    `Guard can be either a function with signature (event) => Boolean or a Boolean.
-     guard: ${String(guard)}
-     event.type: ${event.type}
-     event.payload: ${JSON.stringify(event.payload)} 
-    `
-  )
+  throw new Error(`
+    [internal] evaluateGuard
+    Guard can either be a boolean or function (event) => Boolean, instead received:
+    guard: ${String(guard)} typeof "${typeof guard}"
+    event.type: ${event.type} typeof "${typeof event.type}"
+    event.payload: ${JSON.stringify(event.payload)}
+  `)
 }
 
 const computeNextState = (event: Event) => (state, [path, transformer]: PathTransformerPair): State => {
@@ -71,8 +71,23 @@ export const processEventMap = (pathAndTransformers: PathTransformerPair[], stat
   return pathAndTransformers.reduce(computeNextState(event), state)
 }
 
-const filterItemsByGuard = (event: Event) => ([_, guard]: EventMapItem) => {
-  return evaluateGuard(guard, event)
+const filterItemsByGuard = (event: Event) => (mapItem: EventMapItem) => {
+  const [_, guard] = mapItem
+  if (Array.isArray(mapItem) && mapItem.length > 1) {
+    return evaluateGuard(guard, event)
+  }
+
+  throw new Error(`
+    [internal] filterItemsByGuard
+    Invalid mapItem, expected list with signature [PathItem, Guard, Transformer],
+    instead received:
+
+    mapItem: ${JSON.stringify(mapItem)}, typeof: "${typeof mapItem}"
+    mapItem[0]: ${JSON.stringify(mapItem[0])}, typeof: "${typeof mapItem[0]}"
+    mapItem[1]: ${JSON.stringify(mapItem[1])}, typeof: "${typeof mapItem[1]}"
+    mapItem[2]: ${JSON.stringify(mapItem[2])}, typeof: "${typeof mapItem[2]}"
+
+  `)
 }
 
 const resolveEventMapItem = (eventMap: EventMap, state: State, eventType: EventType) => ([
@@ -87,12 +102,12 @@ const resolveEventMapItem = (eventMap: EventMap, state: State, eventType: EventT
   }
 
   throw new Error(`
-    [internal] validateAndComputePathTransformerPair
+    [internal] resolveEventMapItem
     Something went wrong, this path shouldn‘t be taken. Run the code in debug mode
-    currentPath: ${Array.isArray(currentPath) ? currentPath.join(',') : 'Not an Array'}
-    eventType: ${eventType}
-    path: ${path}
-    transformer: ${transformer}
+    path: "${String(path)}" typeof: "${typeof path}"
+    event.type: "${String(event.type)}" typeof: "${typeof event.type}"
+    transformer: "${String(transformer)}" typeof: "${typeof transformer}"
+    state: ${JSON.stringify(state)}
   `)
 }
 
