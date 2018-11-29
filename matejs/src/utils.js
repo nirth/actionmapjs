aed64f56f989162d6313c6959f2cf38cdcab6fe1
David Grigoryan <d4@h3x.co>
2018-11-29 22:32:47 +0000

    `eventMap` function
    Added function `eventMap`, dead simple function that can create array of `EventMapItems`. This function existense is mainly predicated by aestethics - it’s easier to read:
    ```
    const map = eventMap(
      [‘users’, guard, eventMap(
        [userById, guard, updateUser]
     )]
    )
    ```
    than:
    ```
    const map = [
      [‘users’, guard, [
        [userById, guard, updateUser]
     ]]
    ]
    ```

--- matejs/src/utils.js
+++ matejs/src/utils.js
@@ -1,11 +1,14 @@
 // @flow
-import type {Event, EventType, Guard, Payload} from './types'
+import type {Event, EventType, EventMap, EventMapItem, Guard, Payload} from './types'
 
-export const allowEventType = (allowedEventType: Guard) => ({type, payload}: Event): boolean => type === allowedEventType
+export const allowEventType = (allowedEventType: Guard) => ({type, payload}: Event): boolean =>
+  type === allowedEventType
 
 export const createSimpleEvent = (type: EventType, payload: Payload = null): Event => ({type, payload})
 
 export const simpleEventFactory = (type: EventType) => (payload: Payload): Event => createSimpleEvent(type, payload)
+
+export const eventMap = (...items: EventMapItem): EventMap => [...items]
 
 export const trace = (target: any, index: any) => {
   console.log(`Trace ${index}: ${target}`)
